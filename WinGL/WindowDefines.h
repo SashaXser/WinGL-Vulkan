96cf0e007f79cf54b56c73c4b8c4abe52fbfca99
Ryan Kawicki <Ryan.H.Kawicki@alumni.purdue.edu>
2012-01-15 05:08:37 -0600

    Missing Header File

---
+++ WinGL/WindowDefines.h
@@ -0,0 +1,11 @@
+#ifndef _WINDOW_DEFINES_H_
+#define _WINDOW_DEFINES_H_
+
+// defines a size structure
+struct Size
+{
+   unsigned long  width;
+   unsigned long  height;
+};
+
+#endif // _WINDOW_DEFINES_H_
