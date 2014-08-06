89cb337d4734baee15ee228b80cf40f4023c6f51
Ryan Kawicki <Ryan.H.Kawicki@alumni.purdue.edu>
2014-08-06 02:08:30 -0500

    Still Understanding Tessellation
    - not sure why all of this works...
    - not sure why the triangle is not bent over after rotation...

--- TessellationTriangle/TessellationTriangleWindow.cpp
+++ TessellationTriangle/TessellationTriangleWindow.cpp
@@ -62,10 +62,13 @@ bool TessellationTriangleWindow::Create( unsigned int nWidth,
 // hack begin
 const char * const pVertSrc =
    "#version 410\n" \
-   "uniform mat4 mvp;\n" \
    "layout (location = 0) in vec3 vert_position;\n" \
+   "uniform mat4 mvp;\n" \
+   "out vec4 vert_world_pos_cs;\n" \
    "void main( )\n" \
    "{\n" \
+   //"vert_world_pos_cs = vec4(vert_position, 1.0f);\n" \
+
    "gl_Position = mvp * vec4(vert_position, 1.0f);\n" \
    "}";
 const char * const pFragSrc =
@@ -75,8 +78,38 @@ const char * const pFragSrc =
    "{\n" \
    "FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n" \
    "}";
+const char * const pTCtrlSrc =
+   "#version 410\n" \
+   "layout (vertices = 3) out;\n" \
+   "in vec4 vert_world_pos_cs[];\n" \
+   "out vec4 vert_world_pos_es[];\n" \
+   "uniform vec3 outer;\n" \
+   "uniform float inner;\n" \
+   "void main( )\n" \
+   "{\n" \
+   "gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;\n" \
+   "if (gl_InvocationID==0)\n" \
+   "{\n" \
+   "gl_TessLevelOuter[0] = outer.x;\n" \
+   "gl_TessLevelOuter[1] = outer.y;\n" \
+   "gl_TessLevelOuter[2] = outer.z;\n" \
+   "gl_TessLevelInner[0] = inner;\n" \
+   "}\n" \
+   "}\n";
+const char * const pTEvalSrc =
+   "#version 410\n" \
+   "uniform mat4 mvp;\n" \
+   "layout (triangles, equal_spacing, ccw) in;\n" \
+   "in vec4 vert_world_pos_es[];\n" \
+   "void main( )\n" \
+   "{\n" \
+   //"gl_Position = mvp * vec4((gl_TessCoord.x * vert_world_pos_es[0].xyz), 1.0f);\n" \
+
+   "gl_Position = mvp * gl_in[0].gl_Position * gl_TessCoord.x + gl_in[1].gl_Position * gl_TessCoord.y + gl_in[2].gl_Position * gl_TessCoord.z;\n" \
+   "}\n";
 #include "Matrix.h"
 #include "Shaders.h"
+#include "Timer.h"
 // hack end
 
 int TessellationTriangleWindow::Run( )
@@ -91,12 +124,24 @@ int TessellationTriangleWindow::Run( )
       if (!(bQuit = PeekAppMessages(appQuitVal)))
       {
          // hack job begin
+         glClear(GL_COLOR_BUFFER_BIT);
+         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
          mTriVertArray.Bind();
          glUseProgram(mTriProgID);
-         Matrixf mvp = Matrixf::Ortho(-2.0f, 2, -2, 2, -100, 100) * Matrixf::LookAt(0.0f, 0, -10, 0, 0, 0, 0, 1, 0);
+         static float rot = 0.0f; rot += 0.01f;
+         Matrixf mvp = Matrixf::Ortho(-1.2f, 1.2f, -1.2, 1.2, -100, 100) * Matrixf::LookAt(0.0f, 0, -10, 0, 0, 0, 0, 1, 0) * Matrixf::Rotate(rot, 0, 1, 0);
          GLint loc = glGetUniformLocation(mTriProgID, "mvp");
+         GLint inner = glGetUniformLocation(mTriProgID, "inner");
+         float outer_values[] = { std::abs(std::sin(Timer().GetCurrentTimeSec())) * 1, 
+                                std::abs(std::sin(Timer().GetCurrentTimeSec())) * 1,
+                                std::abs(std::sin(Timer().GetCurrentTimeSec())) * 1};
+         GLint outer = glGetUniformLocation(mTriProgID, "outer");
+         float inner_value = (std::abs(std::sin(Timer().GetCurrentTimeSec()))) * 1;
+         glUniform3fv(outer, 1, outer_values);
+         glUniform1f(inner, inner_value);
          glUniformMatrix4fv(loc, 1, GL_FALSE, mvp);
-         glDrawArrays(GL_LINE_LOOP, 0, 3);
+         glPatchParameteri(GL_PATCH_VERTICES, 3);
+         glDrawArrays(GL_PATCHES, 0, 3);
          glUseProgram(0);
          mTriVertArray.Unbind();
          SwapBuffers(GetHDC());
@@ -129,7 +174,9 @@ void TessellationTriangleWindow::InitShaders( )
    mTriProgID = glCreateProgram();
    GLuint vert = shader::LoadShaderSrc(GL_VERTEX_SHADER, pVertSrc);
    GLuint frag = shader::LoadShaderSrc(GL_FRAGMENT_SHADER, pFragSrc);
-   shader::LinkShaders(mTriProgID, vert, 0, frag);
+   GLuint tctrl = shader::LoadShaderSrc(GL_TESS_CONTROL_SHADER, pTCtrlSrc);
+   GLuint teval = shader::LoadShaderSrc(GL_TESS_EVALUATION_SHADER, pTEvalSrc);
+   shader::LinkShaders(mTriProgID, vert, 0, frag, tctrl, teval);
 
    // hack job end
 
@@ -152,7 +199,7 @@ void TessellationTriangleWindow::InitVertices( )
    mTriVertBuffer.GenBuffer();
    mTriVertBuffer.Bind();
    mTriVertBuffer.BufferData(sizeof(vertices), vertices, GL_STATIC_DRAW);
-   mTriVertBuffer.VertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+   mTriVertBuffer.VertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(*vertices), 0);
 
    // enable vertex array index 0
    mTriVertArray.EnableVertexAttribArray(0);
