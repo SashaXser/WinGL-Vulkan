c2737f70027a3fbd4b43939324aa137164c11a7e
Ryan Kawicki <Ryan.H.Kawicki@alumni.purdue.edu>
2014-09-24 01:06:54 -0500

    Minor Updates To The CMake Files For Assimp Updates

--- ext-src/cmake-modules/FindZLIB.cmake
+++ ext-src/cmake-modules/FindZLIB.cmake
@@ -4,7 +4,7 @@
 include(FindPackageHandleStandardArgs)
 
 # find the directory to which zlib points
-find_file(ZLIB_INCLUDE_DIR
+find_path(ZLIB_INCLUDE_DIR
           zlib.h
           PATHS "${CMAKE_SOURCE_DIR}/ext-src/resil-1.8.2/input libs/zlib-1.2.8"
 #                "${CMAKE_SOURCE_DIR}/ext-src/assimp-3.0.1270/contrib/zlib"
@@ -21,6 +21,7 @@ set(SKIP_INSTALL_ALL YES)
 # since zlib is being compiled by this source tree,
 # use the name of the project to setup the zlib name
 set(ZLIB_LIBRARY zlibstatic CACHE INTERNAL "Library project name for zlib")
+set(ZLIB_LIBRARIES zlibstatic CACHE INTERNAL "Library project name for zlib")
 
 # confirm the location of The include directories and project name
-find_package_handle_standard_args(ZLIB DEFAULT_MSG ZLIB_INCLUDE_DIR ZLIB_LIBRARY)
+find_package_handle_standard_args(ZLIB DEFAULT_MSG ZLIB_INCLUDE_DIR ZLIB_LIBRARY ZLIB_LIBRARIES)
