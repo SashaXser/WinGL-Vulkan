fe8d9b58e79cf1ef2b918297e99185ec75caaef6
Ryan Kawicki <Ryan.H.Kawicki@alumni.purdue.edu>
2020-03-06 22:40:39 -0600

    Adding New Context Functions To Replace Existing

--- Vulkan/libraries/vkl/vkl_context_data.h
+++ Vulkan/libraries/vkl/vkl_context_data.h
@@ -45,6 +45,7 @@ reinterpret_static_cast( const F f )
 
 template <
    size_t CONTEXT_SIZE >
+[[deprecated]]
 inline context_ptr_t AllocateContext( )
 {
    return
@@ -52,8 +53,53 @@ inline context_ptr_t AllocateContext( )
 }
 
 template <
+   typename ContextT,
+   typename ContextDataT,
+   typename ... ContextDataArgsT >
+inline ContextT * AllocateContext(
+   ContextDataArgsT && ... args )
+{
+   context_t * context =
+      new (std::nothrow) context_t[3] { };
+
+   if (context)
+   {
+      ContextDataT * const context_data =
+         new (std::nothrow) ContextDataT {
+            std::forward< ContextDataArgsT >(args)...
+         };
+
+      if (!context_data)
+      {
+         delete [] context; context = nullptr;
+      }
+      else
+      {
+         *reinterpret_cast< ContextDataT ** >(
+            context) = context_data;
+
+         const auto delete_context_data =
+            [ ] ( const ContextDataT * const context_data )
+            {
+               delete context_data;
+            };
+
+         *reinterpret_cast<
+            void (**) ( const ContextDataT * const ) >(
+               context + 1) = delete_context_data;
+      }
+   }
+
+   return
+      context ?
+      reinterpret_cast< ContextT * >(context + 2) :
+      nullptr;
+}
+
+template <
    size_t CONTEXT_SIZE,
    typename T >
+[[deprecated]]
 inline void DeallocateContext(
    const T * const context )
 {
@@ -62,9 +108,34 @@ inline void DeallocateContext(
          context) - (CONTEXT_SIZE - 1));
 }
 
+inline void DeallocateContext(
+   const void * const context )
+{
+   if (context)
+   {
+      const auto context_data =
+         reinterpret_cast< const context_t * >(
+            context) - 2;
+
+      const auto delete_context_data =
+         reinterpret_cast<
+            void (* const) ( const void * const ) >(
+               *(context_data + 1));
+
+      if (delete_context_data)
+      {
+         delete_context_data(
+            reinterpret_cast< void * >(*context_data));
+      }
+
+      delete [] context_data;
+   }
+}
+
 template<
    size_t CONTEXT_SIZE,
    typename T >
+[[deprecated]]
 inline T * GetContextPointer(
    const context_ptr_t context )
 {
@@ -77,6 +148,7 @@ template <
    size_t INDEX,
    size_t CONTEXT_SIZE,
    typename T >
+[[deprecated]]
 inline void SetContextData(
    const context_ptr_t context,
    const T data )
@@ -95,6 +167,7 @@ template <
    typename T,
    size_t INDEX,
    typename C >
+[[deprecated]]
 inline T GetContextData(
    const C * const context )
 {
@@ -106,6 +179,49 @@ inline T GetContextData(
          *reinterpret_cast< const T * >(
             reinterpret_cast< const context_t * >(
                context) - INDEX);
+   }
+
+   return data;
+}
+
+template <
+   typename ContextDataT,
+   typename ContextT >
+inline ContextDataT * GetContextData(
+   ContextT * const context )
+{
+   ContextDataT * context_store { };
+
+   if (context && *context && *(context - 2))
+   {
+      context_store =
+         reinterpret_cast< ContextDataT * >(
+            *(context - 2));
+   }
+
+   return context_store;
+}
+
+template <
+   typename ContextT,
+   typename ContextDataT,
+   typename MemberVariableT >
+inline MemberVariableT GetContextData(
+   const ContextT * const context,
+   MemberVariableT ContextDataT::* const context_data )
+{
+   MemberVariableT data { };
+
+   if (context_data)
+   {
+      const auto * const context_store =
+         GetContextData< ContextDataT >(
+            context);
+
+      if (context_store)
+      {
+         data = context_store->*context_data;
+      }
    }
 
    return data;
